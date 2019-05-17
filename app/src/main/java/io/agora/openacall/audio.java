8ece7eb0a33f0e02b508e06928081ec060a230e6
ניצן לוכסמבורג <nitzanlux@gmail.com>
2019-05-17 08:27:44 +0300

    hand jestuher מחוות יד יעני נו its the class of the listener

--- app/src/main/java/io/agora/openacall/audio.java
+++ app/src/main/java/io/agora/openacall/audio.java
@@ -11,7 +11,17 @@ import io.agora.openacall.ui.OnSwipeTouchListener;
 public class audio extends AppCompatActivity {
     MediaPlayer mp;
     MediaPlayer po;
+    MediaPlayer po1;
+    MediaPlayer po2;
+    MediaPlayer po3;
+    MediaPlayer po4;
+    MediaPlayer po5;
+    MediaPlayer po6;
+    MediaPlayer po7;
+    MediaPlayer temp;
     ConstraintLayout swipeLayout;
+    boolean bar;
+    int index = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +31,17 @@ public class audio extends AppCompatActivity {
         po = MediaPlayer.create(audio.this, R.raw.podcast);
         Bundle extras = getIntent().getExtras();
         boolean t = extras.getBoolean("bool");
+        bar=t;
         if(t){
             mp.start();
             swipeLayout.setBackgroundResource(R.drawable.music_back);
+            po1 =MediaPlayer.create(audio.this,R.raw.c1);
+            po2 =MediaPlayer.create(audio.this,R.raw.c2);
+            po3 =MediaPlayer.create(audio.this,R.raw.c3);
+            po1 =MediaPlayer.create(audio.this,R.raw.c4);
+            po1 =MediaPlayer.create(audio.this,R.raw.c5);
+            po1 =MediaPlayer.create(audio.this,R.raw.c6);
+            po1 =MediaPlayer.create(audio.this,R.raw.c7);
         }else {
             po.start();
             swipeLayout.setBackgroundResource(R.drawable.podcast_back);
@@ -37,6 +55,52 @@ public class audio extends AppCompatActivity {
             }
             @Override
             public void onDoubleClick(){
+                index = (index+1)%8;
+                if(bar){
+                    switch (index){
+                        case 0:
+                            if(po7.isPlaying()){
+                                po7.stop();
+                            }
+                            mp.start();
+                            temp= mp;
+                        break;
+                        case 1:
+                            mp.stop();
+                            po1.start();
+                            temp=po1;
+                        break;
+                        case 2:
+                            po1.stop();
+                            po2.start();
+                            temp = po2;
+                        break;
+                        case 3:
+                            po2.stop();
+                            po3.start();
+                            temp= po3;
+                            break;
+                        case 4:
+                            po3.stop();
+                            po4.start();
+                            temp = po4;
+                            break;
+                        case 5:
+                            po4.stop();
+                            po5.start();
+                            temp = po5;
+                            break;
+                        case 6:
+                            po5.stop();
+                            po6.start();
+                            temp =po6;
+                            break;
+                        case 7:
+                            po6.stop();
+                            po7.start();
+                            temp = po7;
+                    }
+                }
             }
         });
     }
@@ -44,6 +108,9 @@ public class audio extends AppCompatActivity {
     @Override
     public void onStop () {
 //do your stuff here
+        if(temp.isPlaying()){
+            temp.stop();
+        }
         if(mp.isPlaying()){
             mp.stop();
         }
